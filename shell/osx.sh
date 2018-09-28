05f43ca848b8b5f5bd7b4cadb9c121e4c2e3e78a
jianoli <jianoli@gmail.com>
2018-09-29 00:02:06 +0800

    mouse acceleration

--- shell/osx.sh
+++ shell/osx.sh
@@ -4,3 +4,6 @@ sudo systemsetup -gettimezone
 # hidden files
 defaults write com.apple.finder AppleShowAllFiles TRUE
 killall Finder
+
+# turn off mouse acceleration
+defaults write .GlobalPreferences com.apple.mouse.scaling -1
